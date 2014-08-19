fc4ff9747ee408cf86acd51a408bbdb8764192b7
Jan Lolling <jan.lolling@gmail.com>
2014-08-19 12:43:01 +0000

    initial

---
+++ src/main/java/de/cimt/talendcomp/tac/GetTaskStatus.java
@@ -0,0 +1,69 @@
+package de.cimt.talendcomp.tac;
+
+public class GetTaskStatus extends TaskAction {
+	
+	public static final String ACTION_NAME = "getTaskStatus";
+	private String errorStatus;
+	private String status;
+	private Integer returnCode;
+
+	@Override
+	public String getAction() {
+		return ACTION_NAME;
+	}
+
+	@Override
+	public void execute() throws Exception {
+		addParam(PARAM_TASKID, taskId);
+		String result = executeRequest();
+		if (result != null && result.trim().startsWith(ERROR)) {
+			// error by processing the request
+			throw new Exception("Getting task status failed: " + result);
+		} else {
+			errorStatus = Util.extractByRegexGroup(result, "\"errorStatus\":\"([A-Z_a-z0-9]*)\"", 1);
+			if (errorStatus != null) {
+				errorStatus = errorStatus.trim();
+			}
+			status = Util.extractByRegexGroup(result, "\"status\":\"([A-Z_a-z0-9]*)\"", 1);
+			if (NO_ERROR.equals(errorStatus) == false) {
+				returnCode = 4; // default return code in case of error
+			} else {
+				returnCode = null;
+			}
+		}
+	}
+	
+	public boolean isRunning() {
+		// running or requesting stop (it is running yet)
+		return REQUESTING_RUN.equals(status) || RUNNING.equals(status) || REQUESTING_STOP.equals(status);
+	}
+	
+	public boolean isReadyToRun() {
+		return READY_TO_RUN.equals(status);
+	}
+
+	public boolean hasErrors() {
+		return NO_ERROR.equals(errorStatus) == false;
+	}
+
+	public String getErrorStatus() {
+		return errorStatus;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+	
+	public Integer getReturnCode() {
+		return returnCode;
+	}
+	
+	public boolean isPreparing() {
+		if (status != null) {
+			return (status.contains(GENERATE_PATTERN) || status.contains(DEPLOY_PATTERN) || status.contains(SENDING_SCRIPT_PATTERN)) && hasErrors() == false;
+		} else {
+			return false;
+		}
+	}
+
+}
---
+++ src/main/java/de/cimt/talendcomp/tac/RunTask.java
@@ -0,0 +1,115 @@
+package de.cimt.talendcomp.tac;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunTask extends TaskAction {
+	
+	public static final String ACTION_NAME = "runTask";
+	public static final String PARAM_MODE = "mode";
+	public static final String PARAM_CONTEXT = "context"; 
+	private String errorStatus;
+	private boolean synchronous = true;
+	private Map<String, Object> contextParams = new HashMap<String, Object>();
+	private String status;
+	private Integer returnCode;
+	private SimpleDateFormat date_param_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Override
+	public String getAction() {
+		return ACTION_NAME;
+	}
+
+	@Override
+	public void execute() throws Exception {
+		addParam(PARAM_TASKID, taskId);
+		addParam(PARAM_MODE, synchronous ? "synchronous" : "asynchronous");
+		if (contextParams.isEmpty() == false) {
+			addParam(PARAM_CONTEXT, buildContextParamJson());
+		}
+		String result = executeRequest();
+		if (result != null && result.trim().startsWith(ERROR)) {
+			// error by processing the request
+			throw new Exception("Starting task failed: " + result);
+		} else {
+			if (synchronous) {
+				errorStatus = Util.extractByRegexGroup(result, "\"errorStatus\":\"([A-Z_a-z0-9]*)\"", 1);
+				if (errorStatus != null) {
+					errorStatus = errorStatus.trim();
+				}
+				status = Util.extractByRegexGroup(result, "\"status\":\"([A-Z_a-z0-9]*)\"", 1);
+				if (NO_ERROR.equals(errorStatus) == false) {
+					returnCode = 4;
+				} else {
+					returnCode = null;
+				}
+			}
+		}
+	}
+
+	public boolean isSynchronous() {
+		return synchronous;
+	}
+
+	public void setSynchronous(boolean synchronous) {
+		this.synchronous = synchronous;
+	}
+	
+	public void addContextParam(String key, Object value) {
+		if (key != null && key.isEmpty() == false && value != null) {
+			contextParams.put(key, value);
+		}
+	}
+	
+	private String buildContextParamJson() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		boolean firstLoop = true;
+		for (Map.Entry<String, Object> entry : contextParams.entrySet()) {
+			if (firstLoop) {
+				firstLoop = false;
+			} else {
+				sb.append(",");
+			}
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\":");
+			sb.append(convertValue(entry.getValue()));
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+		
+	private String convertValue(Object value) {
+		if (value != null) {
+			if (value instanceof Date) {
+				return "\"" + date_param_format.format((Date) value) + "\"";
+			} else if (value instanceof String) {
+				String s = ((String) value).replace("\\", "\\\\");
+				s = s.replace("\n", "\\n");
+				s = s.replace("\r", "\\r");
+				s = s.replace("\t", "\\t");
+				return "\"" + s + "\"";
+			} else {
+				return "\"" + value.toString() + "\"";
+			}
+		} else {
+			return "";
+		}
+	}
+
+	public String getStatus() {
+		return status;
+	}
+	
+	public Integer getReturnCode() {
+		return returnCode;
+	}
+
+	public String getErrorStatus() {
+		return errorStatus;
+	}
+
+}
